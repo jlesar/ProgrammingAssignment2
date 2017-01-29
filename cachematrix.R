0e8535ecfd499c0795d5a06b77ac18f9e9340f2b
jlesar <jlesar@gmail.com>
2017-01-29 01:31:42 -0500

    Wk2assignment-recommit

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,53 @@
 ##setorig, getorig -- so swapping in new matrix will use testVector$setorig 
 ##rather than testVector$set
 
+##To create a vector that can store a matrix and its inverse
+##create 'new' matrix, make sure m (cache) is empty
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- matrix()
+    m <- NULL
+   
+    ##set input to x, reset cache 
+    setorig <- function(y) {
+        x <<- y
+        m <<- NULL
+      }
+  
+    ##retrieve original (input) matrix value
+    getorig<- function() x 
+    
+    ##calculate inverse matrix
+    setinverse <- function(invmatrix) m <<- invmatrix
 
-}
+    
+    ##retrieve inverse matrix
+    getinverse <- function() m
+    
+    ##output is list of 4 elements to be used by cacheSolve    
+        list(setorig = setorig, getorig = getorig,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  }
+  
 
-
-## Write a short comment describing this function
-
+## To return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+    ## Look for cached inverse; if exists, notify user and return
+  
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+  
+    ##If not cached, calculate inverse, store as m and return
+    
+    data <- x$getorig()
+    m <- solve(data)
+    x$setinverse(m)
+    m
+  }
+  
+
